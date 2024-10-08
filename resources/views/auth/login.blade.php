228a399f18d6a4fbc38afefe944e13070f274fd9
Matteo Trotta <matteotrotta7@gmail.com>
2024-10-08 17:22:19 +0200

    form ok

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,31 +11,41 @@
     </div>
 
 
-    <div class="container">
-        <div class="row justift content-center align-items-center">
-            <div class="col-12">
 
-                <form>
+    <div class="container-fluid">
+        <div class="row justify-content-center my-3">
+            <div class="col-10">
+
+                <form action="{{ route('register') }}" method="POST" class="shadow-lg rounded-3 p-4 my5">
+                    @csrf
+
                     <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                            placeholder="Enter email">
+
+                        <label for="email">Email address</label>
+                        <input type="email" class="form-control" id="email" name="email"
+                            aria-describedby="emailHelp">
+
                         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
                             else.</small>
+
                     </div>
+
                     <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" name="password">
                     </div>
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+
+
+
+
+                    <button type="submit" class="btn btn-primary p-2 my-2">Submit</button>
+
                 </form>
 
             </div>
         </div>
     </div>
+
+
 
 </x-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
 
     <div class="container">
         <div class="row text-center">
-            <div class="col-10">
+            <div class="col-10 my-3">
 
                 <h2>Register form</h2>
 
@@ -10,42 +10,53 @@
         </div>
     </div>
 
+    <div class="container-fluid">
+        <div class="row justify-content-center my-3">
+            <div class="col-10">
 
-    <form action="{{ route('register') }}" method="POST">
-        @csrf
+                <form action="{{ route('register') }}" method="POST" class="shadow-lg rounded-3 p-4 my5">
+                    @csrf
 
-        <div class="form-group">
+                    <div class="form-group">
 
-            <label for="email">Email address</label>
-            <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp"
-                placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        <label for="email">Email address</label>
+                        <input type="email" class="form-control" id="email" name="email"
+                            aria-describedby="emailHelp">
+
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
+                            else.</small>
+
+                    </div>
+
+                    <div class="form-group">
+                        <label for="text">Nome</label>
+                        <input type="text" class="form-control" id="nome" name="nome">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="text">Cognome</label>
+                        <input type="text" class="form-control" id="cognome" name="cognome">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" name="password">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password_confirmation">Password Confirmation</label>
+                        <input type="password" class="form-control" id="password_confirmation" id="password"
+                            name="password_confirmation">
+                    </div>
+
+
+                    <button type="submit" class="btn btn-success p-2 my-2">Submit</button>
+
+                </form>
+
+            </div>
         </div>
+    </div>
 
-        <div class="form-group">
-            <label for="text">Nome</label>
-            <input type="text" class="form-control" id="nome" placeholder="Nome" name="nome">
-        </div>
-
-        <div class="form-group">
-            <label for="text">Cognome</label>
-            <input type="text" class="form-control" id="cognome" placeholder="cognome" name="cognome">
-        </div>
-
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" class="form-control" id="password" placeholder="Password" name="password">
-        </div>
-
-        <div class="form-group">
-            <label for="password_confirmation">Password Confirmation</label>
-            <input type="password" class="form-control" id="password_confirmation" placeholder="Password Confirmation"
-                id="password" name="password_confirmation">
-        </div>
-
-
-        <button type="submit" class="btn btn-primary">Submit</button>
-
-    </form>
 
 </x-layout>
