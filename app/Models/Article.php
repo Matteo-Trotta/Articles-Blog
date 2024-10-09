79892b964d1d1c6723cfc0208766896d0b782629
Matteo Trotta <matteotrotta7@gmail.com>
2024-10-09 11:22:36 +0200

    relazioni-articles-cattegory

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,10 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
     use HasFactory;
 
     protected $fillable=['title','subtitle','body','image','user_id','category_id'];
+
+    public function user(){
+        return $this->belongTo(User::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
