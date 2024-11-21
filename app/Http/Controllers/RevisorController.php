4cddd04ad58f4625ca68d9fbbd5abf7bd3dcd9a8
Matteo Trotta <matteotrotta7@gmail.com>
2024-11-21 16:11:30 +0100

    dashboard revisor ok

--- app/Http/Controllers/RevisorController.php
+++ app/Http/Controllers/RevisorController.php
@@ -11,8 +11,8 @@ class RevisorController extends Controller
     {
 
         $unrevisionedArticles = Article::where('is_accepted', NULL)->get();
-        $acceptedArticles = Article::where('is_accepted', true)->get;
-        $rejectedArticles = Article::where('is_accepted', false)->get;
+        $acceptedArticles = Article::where('is_accepted', true)->get();
+        $rejectedArticles = Article::where('is_accepted', false)->get();
 
         return view('revisor.dashboard', compact('unrevisionedArticles', 'acceptedArticles', 'rejectedArticles'));
     }
