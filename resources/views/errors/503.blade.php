60ecd02bbc19a1b35e055887a88c1c8dc184ffaa
Matteo Trotta <matteotrotta7@gmail.com>
2025-06-05 14:16:34 +0200

    errore 503 modificata view

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -4,15 +4,28 @@
     <meta charset="UTF-8">
     <title>Sito in Manutenzione</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet"> <!-- opzionale -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"> 
 </head>
 <body class="d-flex justify-content-center align-items-center vh-100 bg-light text-center">
 
-    <div>
-        <h1 class="display-4 text-danger">🚧 Sito in manutenzione</h1>
-        <p class="fs-4">Stiamo aggiornando il nostro blog per offrirti un'esperienza migliore.<br> Riprova più tardi!</p>
-        <p class="text-muted">Torna a trovarci presto 👨‍💻</p>
+
+
+
+<div class="continer">
+    <div class="row text-center">
+        <div class="col-12">
+
+            <div>
+                <h1 class="display-4 text-danger">🚧 Sito in manutenzione</h1>
+                <p class="fs-4">Stiamo aggiornando il nostro blog per offrirti un'esperienza migliore.<br> Riprova più tardi!</p>
+                <p class="text-muted">Torna a trovarci presto 👨‍💻</p>
+            </div>
+
+        </div>
     </div>
+</div>
+
+    
 
 </body>
 </html>
