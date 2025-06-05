370bc8205d1d997900733e427229c0308acbfe43
Matteo Trotta <matteotrotta7@gmail.com>
2025-06-05 14:13:06 +0200

    gestione vista errore 503

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <title>Sito in Manutenzione</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"> <!-- opzionale -->
+</head>
+<body class="d-flex justify-content-center align-items-center vh-100 bg-light text-center">
+
+    <div>
+        <h1 class="display-4 text-danger">🚧 Sito in manutenzione</h1>
+        <p class="fs-4">Stiamo aggiornando il nostro blog per offrirti un'esperienza migliore.<br> Riprova più tardi!</p>
+        <p class="text-muted">Torna a trovarci presto 👨‍💻</p>
+    </div>
+
+</body>
+</html>
